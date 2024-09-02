e9ee206a2914c1d9429de2edfe89876366645211
barathraj10 <barathraj.offl@gmail.com>
2024-09-02 10:58:14 +0530

    Updated files and added new content

--- src/content.js
+++ src/content.js
@@ -3,8 +3,7 @@ import './App.css';
 
 function Content(){
     return(
-        <div class="container bg">
-            
+        <div class="container bg">            
             <img src="https://st3.depositphotos.com/11245678/36021/i/450/depositphotos_360215620-stock-photo-breaking-news-rendering-virtual-set.jpg"></img>
         </div>
     )
